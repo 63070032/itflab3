ffea4ae89d5a8716e9534acdd8b317624bbba098
63070032 <73679716+63070032@users.noreply.github.com>
2020-11-20 19:40:24 +0700

    Update show2.php

--- show2.php
+++ show2.php
@@ -20,7 +20,7 @@ if (mysqli_connect_errno($conn))
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
-$res = mysqli_query($conn, 'SELECT * FROM guestbook');
+$res = mysqli_query($conn, 'SELECT * FROM guestbook1');
 ?>
 
 <div class="container">
